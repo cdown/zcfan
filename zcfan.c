45c94f56e4e6572bda093c6af8ea998f6f5a5f36
Chris Down <chris@chrisdown.name>
2022-03-23 11:51:43 -0400

    Move to use expect() macro everywhere

--- zcfan.c
+++ zcfan.c
@@ -1,22 +1,27 @@
-#include <assert.h>
 #include <errno.h>
 #include <glob.h>
 #include <limits.h>
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-/* All temp values from sysfs are are in milldegree celsius */
 #define C_TO_MILLIC(n) (n * 1000)
 #define MILLIC_TO_C(n) (n / 1000)
-
 #define TEMP_FILES_GLOB "/sys/class/thermal/thermal_zone*/temp"
 #define FAN_CONTROL_FILE "/proc/acpi/ibm/fan"
 #define TEMP_MAX_MCEL C_TO_MILLIC(150)
 #define TEMP_INVALID INT_MIN
 
-static const char *prog_name = NULL;
+#define expect(x)                                                              \
+    do {                                                                       \
+        if (!(x)) {                                                            \
+            fprintf(stderr, "FATAL: !(%s) at %s:%s:%d\n", #x, __FILE__,        \
+                    __func__, __LINE__);                                       \
+            abort();                                                           \
+        }                                                                      \
+    } while (0)
 
 enum FanLevel {
     FAN_OFF = 0,
@@ -30,7 +35,7 @@ struct Rule {
     enum FanLevel fan_level;
 };
 
-/* Must be ordered from highest to lowest temp */
+/* Must be highest to lowest temp */
 static const struct Rule rules[] = {
     {80, FAN_MAX},
     {75, FAN_MED},
@@ -38,18 +43,12 @@ static const struct Rule rules[] = {
     {0, FAN_OFF},
 };
 
-/*
- * How many degrees lower than the fan level activation speed you must be
- * before you're allowed to switch to a lower fan speed
- */
+static const char *prog_name = NULL;
 static unsigned int fan_hysteresis = 10;
-
-static const struct Rule *current_rule = NULL;
-
 static char output_buf[512];
+static const struct Rule *current_rule = NULL;
 static glob_t temp_files;
 static int temp_files_populated = 0;
-
 static volatile sig_atomic_t run = 1;
 
 static int glob_err_handler(const char *epath, int eerrno) {
@@ -58,16 +57,15 @@ static int glob_err_handler(const char *epath, int eerrno) {
 }
 
 static int read_temp_file(const char *filename) {
-    FILE *f;
+    FILE *f = fopen(filename, "re");
     int val;
 
-    f = fopen(filename, "re");
     if (!f) {
         fprintf(stderr, "%s: fopen: %s\n", filename, strerror(errno));
         return errno;
     }
 
-    assert(fscanf(f, "%d", &val));
+    expect(fscanf(f, "%d", &val));
     fclose(f);
 
     if (val <= 0 || val > TEMP_MAX_MCEL) {
@@ -85,7 +83,7 @@ static int get_max_temp(void) {
 
     if (!temp_files_populated) {
         ret = glob(TEMP_FILES_GLOB, 0, glob_err_handler, &temp_files);
-        if (ret != 0) {
+        if (ret) {
             const char *err = "glob: Unknown error";
             switch (ret) {
                 case GLOB_NOMATCH:
@@ -103,9 +101,7 @@ static int get_max_temp(void) {
     }
 
     for (i = 0; i < temp_files.gl_pathc; i++) {
-        const char *tf = temp_files.gl_pathv[i];
-        int temp = read_temp_file(tf);
-
+        int temp = read_temp_file(temp_files.gl_pathv[i]);
         if (temp > max_temp) {
             max_temp = temp;
         }
@@ -119,21 +115,17 @@ static int get_max_temp(void) {
     return MILLIC_TO_C(max_temp);
 }
 
-static int write_fan_level(const char *level) {
-    FILE *f;
-
-    f = fopen(FAN_CONTROL_FILE, "we");
+static void write_fan_level(const char *level) {
+    FILE *f = fopen(FAN_CONTROL_FILE, "we");
     if (!f) {
         fprintf(stderr, "%s: fopen: %s\n", FAN_CONTROL_FILE, strerror(errno));
-        return errno;
+        return;
     }
 
-    assert(fprintf(f, "level %s", level));
+    expect(fprintf(f, "level %s", level));
     fclose(f);
 
     printf("Set fan level %s\n", level);
-
-    return 0;
 }
 
 #define LEVEL_MAX sizeof("disengaged")
@@ -153,7 +145,6 @@ static void set_fan_level(void) {
         char level[LEVEL_MAX];
 
         if (rule == current_rule) {
-            /* Penalty for all further rules for hysteresis */
             penalty = fan_hysteresis;
         }
 
@@ -161,7 +152,7 @@ static void set_fan_level(void) {
             (rule->threshold - penalty) < (unsigned int)max_temp) {
             if (rule != current_rule) {
                 current_rule = rule;
-                assert((size_t)snprintf(level, sizeof(level), "%d",
+                expect((size_t)snprintf(level, sizeof(level), "%d",
                                         rule->fan_level) < sizeof(level));
                 write_fan_level(level);
             }
@@ -181,9 +172,9 @@ int main(int argc, char *argv[]) {
     sigset_t mask;
 
     sigfillset(&mask);
-    (void)sigaction(SIGTERM, &sa_exit, NULL);
-    (void)sigaction(SIGINT, &sa_exit, NULL);
-    (void)setvbuf(stdout, output_buf, _IOLBF, sizeof(output_buf));
+    expect(sigaction(SIGTERM, &sa_exit, NULL) >= 0);
+    expect(sigaction(SIGINT, &sa_exit, NULL) >= 0);
+    expect(setvbuf(stdout, output_buf, _IOLBF, sizeof(output_buf)) == 0);
 
     prog_name = argv[0];
 
@@ -192,7 +183,7 @@ int main(int argc, char *argv[]) {
         sleep(1);
     }
 
-    (void)sigprocmask(SIG_SETMASK, &mask, NULL);
+    expect(sigprocmask(SIG_SETMASK, &mask, NULL) == 0);
     write_fan_level("auto");
     if (temp_files_populated) {
         globfree(&temp_files);
